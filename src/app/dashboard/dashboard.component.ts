4d762bcf6317a7833f5a242a94544989b30e55e2
mrtnmndc <martin.mondaca2023@twk.cl>
2024-08-20 11:10:20 -0400

    git martin

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -14,11 +14,11 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 export class DashboardComponent implements AfterViewInit {
   cards = [
     { id: 'lineChart1', title: 'Temperatura', /*imageSrc: 'https://images.vexels.com/media/users/3/127021/isolated/preview/c0cf811b6355be6f12fed44bf51e8fd2-termometro-de-temperatura-icon-svg.png'*/},
-    { id: 'lineChart2', title: 'Humedad' },
+    { id: 'lineChart2', title: 'Humedad Suelo' },
     { id: 'barChart1', title: 'Velocidad Viento' },
     { id: 'lineChart3', title: 'Precipitación' },
     { id: 'lineChart4', title: 'Radiación UV' },
-    { id: 'lineChart5', title: 'Humedad Suelo' },
+    { id: 'lineChart5', title: 'Bateria' },
     { id: 'lineChart6', title: 'Caudal Q' },
     { id: 'ultimosValores', title:'Ultimos Valores'}
   ];
