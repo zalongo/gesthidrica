cfb8173a9da917b740404a76f3f0a64dc20ff7ee
Mauricio Antonio Lefno Tiznado <mauricio.lefno2023@twk.cl>
2024-10-08 01:54:20 -0300

    Añadido contenido a calcular-huella

--- src/app/calcular-huella/calcular-huella.component.ts
+++ src/app/calcular-huella/calcular-huella.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-calcular-huella',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './calcular-huella.component.html',
   styleUrl: './calcular-huella.component.css'
 })
 export class CalcularHuellaComponent {
+  currentStep: number = 1;
 
+  // Avanza al siguiente paso
+  nextStep() {
+    if (this.currentStep < 5) {
+      this.currentStep++;
+    }
+  }
+
+  // Retrocede al paso anterior
+  previousStep() {
+    if (this.currentStep > 1) {
+      this.currentStep--;
+    }
+  }
 }
