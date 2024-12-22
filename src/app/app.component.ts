fbb5f5759d9c518e8c88ad8379d32d2996619930
Zalongo <gvenegas@webdinamica.cl>
2024-12-22 10:46:40 -0300

    se pintaros los componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component'; // Asegúrate de que la ruta es correcta
 import { FooterComponent } from './footer/footer.component'; // Asegúrate de que la ruta es correcta
@@ -9,8 +9,11 @@ import { FooterComponent } from './footer/footer.component'; // Asegúrate de qu
   standalone: true,
   imports: [RouterOutlet, CommonModule, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'GestionHidrica';
+  constructor(public router: Router) {
+    console.log(router.url);
+  }
 }
