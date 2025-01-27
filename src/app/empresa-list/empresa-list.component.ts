cd7f8c1e57f8e823271898985c50871986b5ea37
Zalongo <gvenegas@webdinamica.cl>
2025-01-27 12:47:04 -0300

    datos empresa

--- src/app/empresa-list/empresa-list.component.ts
+++ src/app/empresa-list/empresa-list.component.ts
@@ -14,14 +14,14 @@ declare var bootstrap: any;
 
 export class EmpresaListComponent {
   empresas = [
-    // { nombre: 'Agrícola los Pellines', representante: 'Nombre Apellido', comuna: 'Comuna' },
+    { nombre: 'Agrícola los Pellines', representante: 'Nombre Apellido', comuna: 'Perquenco' },
     // { nombre: 'Agricola Jorge Borgeaud', representante: 'Nombre Apellido', comuna: 'Comuna' },
     // { nombre: 'Agrícola Los Puntales', representante: 'Nombre Apellido', comuna: 'Comuna' },
     // { nombre: 'Soc. Inv. Matristica', representante: 'Nombre Apellido', comuna: 'Comuna' },
     // { nombre: 'Agrícola Tomás Echavarri', representante: 'Nombre Apellido', comuna: 'Comuna' },
     // { nombre: 'Agrícola Domingo Guzmán', representante: 'Nombre Apellido', comuna: 'Comuna' },
     // { nombre: 'Agrícola AgroAngol', representante: 'Nombre Apellido', comuna: 'Comuna' },
-    { nombre: 'Agrícola Los Tatas', representante: 'Nombre Apellido', comuna: 'Comuna' },
+    // { nombre: 'Agrícola Los Tatas', representante: 'Nombre Apellido', comuna: 'Comuna' },
     // { nombre: 'Agrícola Santa Marina', representante: 'Nombre Apellido', comuna: 'Comuna' },
     // { nombre: 'Agrícola David Estrada', representante: 'Nombre Apellido', comuna: 'Comuna' }
   ];
