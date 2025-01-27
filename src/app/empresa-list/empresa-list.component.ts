89496ada8c3dd44650f1d0a0de086eee5b5c6c88
Zalongo <gvenegas@webdinamica.cl>
2025-01-27 12:49:16 -0300

    datos empresa

--- src/app/empresa-list/empresa-list.component.ts
+++ src/app/empresa-list/empresa-list.component.ts
@@ -1,4 +1,4 @@
-import { Router } from "@angular/router";
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -9,9 +9,8 @@ declare var bootstrap: any;
   standalone: true,
   imports: [CommonModule],
   templateUrl: './empresa-list.component.html',
-  styleUrls: ['./empresa-list.component.css']
+  styleUrls: ['./empresa-list.component.css'],
 })
-
 export class EmpresaListComponent {
   empresas = [
     // { nombre: 'Agrícola los Pellines', representante: 'Nombre Apellido', comuna: 'Comuna' },
@@ -20,14 +19,18 @@ export class EmpresaListComponent {
     // { nombre: 'Soc. Inv. Matristica', representante: 'Nombre Apellido', comuna: 'Comuna' },
     // { nombre: 'Agrícola Tomás Echavarri', representante: 'Nombre Apellido', comuna: 'Comuna' },
     // { nombre: 'Agrícola Domingo Guzmán', representante: 'Nombre Apellido', comuna: 'Comuna' },
+    {
+      nombre: 'Agricola Jorge Borgeaud',
+      representante: 'Nombre Apellido',
+      comuna: 'Ercilla',
+    },
     // { nombre: 'Agrícola AgroAngol', representante: 'Nombre Apellido', comuna: 'Comuna' },
-    { nombre: 'Agrícola Los Tatas', representante: 'Nombre Apellido', comuna: 'Comuna' },
+    // { nombre: 'Agrícola Los Tatas', representante: 'Nombre Apellido', comuna: 'Comuna' },
     // { nombre: 'Agrícola Santa Marina', representante: 'Nombre Apellido', comuna: 'Comuna' },
     // { nombre: 'Agrícola David Estrada', representante: 'Nombre Apellido', comuna: 'Comuna' }
   ];
 
   selectedEmpresa: any;
-
 
   constructor(private router: Router) {}
 
@@ -43,9 +46,9 @@ export class EmpresaListComponent {
   navigateTo(option: string) {
     const modalElement = document.getElementById('empresaModal');
     if (modalElement) {
-      const modal = bootstrap.Modal.getInstance(modalElement);  // Obtén la instancia del modal
+      const modal = bootstrap.Modal.getInstance(modalElement); // Obtén la instancia del modal
       if (modal) {
-        modal.hide();  // Cierra el modal
+        modal.hide(); // Cierra el modal
       }
     }
 
